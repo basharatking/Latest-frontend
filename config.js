834057501606a06ef6ef96ad8e3760d2b7db8228
basharatking <86537190+basharatking@users.noreply.github.com>
2026-04-26 16:58:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.LIVEPDF_CONFIG = {
-  API_BASE: "https://updated-backend--basharatmasih12.replit.app",
+  API_BASE: "https://latest-backend--basharatmasih12.replit.app",
   FREE_LIMIT_MB: 10,
   VERSION: "4.0"
 };
